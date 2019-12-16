1a439da531a752a1fd33502d6b7baa54451666d1
KedarChandulur <kedarguptachandulur@gmail.com>
2019-12-15 21:16:01 -0800

    AddedMiniMap
    Added MiniMap to Game,
    Changes in Button Colour.

---
+++ MIni Project 4-1/Assets/Scripts/MiniMapIN.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMapIN : MonoBehaviour
+{
+	#region RequiredComponents
+
+	public Transform player;
+	public Transform MiniMapBorder;
+	public bool ChangeMiniMapBorderColor = true;
+	Image MiniMapBorderImage;
+
+	#endregion
+
+	void Start()
+	{
+		if (ChangeMiniMapBorderColor)
+		{
+			MiniMapBorderImage = MiniMapBorder.GetComponent<Image>();
+			MiniMapBorderImage.color = new Color(Random.value, Random.value, Random.value, 255);
+			StartCoroutine(ChangeImageBorderColour());
+		}
+	}
+
+	void LateUpdate()
+	{
+		MiniMap2Player();
+	}
+
+	//Main Code...
+	#region Code
+
+	void MiniMap2Player()
+	{
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+
+		transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+	}
+
+	IEnumerator ChangeImageBorderColour()
+	{
+		yield return new WaitForSeconds(5f);
+		MiniMapBorderImage.color = new Color(Random.value, Random.value, Random.value, 255);
+	}
+
+	#endregion
+}
